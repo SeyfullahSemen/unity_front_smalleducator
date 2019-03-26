fcc5e6d4f09d7fc3661a9765a67a83c755777cf0
Seyfullah <seyfullah.semen@hotmail.com>
2019-03-26 15:33:43 +0100

    Initial commit

---
+++ smalleducator-master/SmallEducator/Assets/CreateLessonScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class CreateLessonScript : MonoBehaviour
+{
+
+    public Button goBackButton;
+    public Button createButton;
+    public InputField lesNaam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /**
+     * Init function to declare the gameobjects
+     */
+    private void init() {
+
+        goBackButton = GameObject.FindGameObjectWithTag("goBackButton").GetComponent<Button>();
+        createButton = GameObject.FindGameObjectWithTag("voegLesToeButton").GetComponent<Button>();
+        lesNaam = GameObject.FindGameObjectWithTag("lesNaamField").GetComponent<InputField>();
+
+    }
+
+  
+    public void goBackOnClick() {
+        SceneManager.LoadScene("SampleScene",LoadSceneMode.Single);
+    }
+
+    public void createLessonOnClick() {
+        string newLesson = lesNaam.text.ToString(); 
+        if (newLesson == "" || newLesson.Length == 0) {
+            Debug.Log("Please enter a valid lesson name into the input field!");
+        }
+        else{
+            StartCoroutine(postNewLesson(newLesson)); // function to create a new lesson
+            Debug.Log("Creating new lesson " + newLesson);
+        }
+        
+    }
+
+
+    IEnumerator postNewLesson(string newLesson) {
+       var uwr = new UnityWebRequest("http://localhost:8080/lesson", "POST"); // connect to the backend
+        
+        string jsonToSend1 = "{\"lessonname\":\"" + newLesson + "\"}";
+        
+       byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonToSend1);
+       uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+       uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+       uwr.SetRequestHeader("Content-Type", "application/json");
+
+        yield return uwr.SendWebRequest();
+
+        // Check if there are network errors
+        if (uwr.isNetworkError || uwr.isHttpError)
+        {
+            Debug.Log(uwr.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+
+    }
+
+}
+
